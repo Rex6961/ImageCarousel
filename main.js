7ab90db639f7f6b5348d5c6938dcdca342dc7a1f
Rex6961 <aon777ar@gmail.com>
2024-11-06 23:22:26 -0300

    Added div element with table for images to the begininng of body. Added crated images to the corresponding table cells. Added previous and next buttons to the end of body.

--- main.js
+++ main.js
@@ -18,7 +18,30 @@ class ImageTag {
     }
 };
 
+// Добавляем div с таблицей для изображений в начало body
+document.body.insertAdjacentHTML('afterbegin', 
+    `<div id="customDiv">
+        <table>
+            <tr>
+                <td id="img1"></td>
+                <td id="img2"></td>
+                <td id="img3"></td>
+            </tr>
+        </table>
+    </div>`);
+
 // Создаем элементы изображений с помощью класса ImageTag
 const img1 = new ImageTag('img').createElem({'src': image[image.length - 1], 'alt': 'flowers'});
 const img2 = new ImageTag('img').createElem({'src': image[0], 'alt': 'flowers'});
 const img3 = new ImageTag('img').createElem({'src': image[1], 'alt': 'flowers'});
+
+// Добавляем созданные изображения в соответствующие ячейки таблицы
+document.querySelector('#img1').appendChild(img1);
+document.querySelector('#img2').appendChild(img2);
+document.querySelector('#img3').appendChild(img3);
+
+// Добавляем кнопки "previous" и "next" в конец body
+document.body.insertAdjacentHTML('beforeend',
+    `<button type="button" id="previousBtn">previous</button><button type="button" id="nextBtn">next</button>`
+);
+
